974b705006c8ddbc6853f37f39d1dd40dbae0550
Oluwaseun <annyauthe4@gmail.com>
2023-10-05 20:12:32 -0700

    Display message

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/* This program display message using. */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
