3249c5972cab1e874a294f34d29d7ddcf54a226f
Oluwaseun <annyauthe4@gmail.com>
2023-10-08 14:50:59 -0700

    Print complex number combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 99; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar('0' + (i / 10));
+			putchar('0' + (i % 10));
+			putchar(' ');
+			putchar('0' + (j / 10));
+			putchar('0' + (j % 10));
+			if (i != 98 || j != 99)
+			{
+				putchar('.');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
