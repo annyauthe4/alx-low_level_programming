80923ca0dd859f10586a8613aa20fee6d8b20651
Oluwaseun <annyauthe4@gmail.com>
2023-10-11 23:37:55 -0700

    Print 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,10 +25,15 @@ void times_table(void)
 				_putchar('0' + (result / 10));
 				_putchar('0' + (result % 10));
 			}
-			if (j < 9)
+			if (j < 9 && result < 10)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (j < 9 && result >= 10)
+			{
+				_putchar(',');
 				_putchar(' ');
 			}
 		}
