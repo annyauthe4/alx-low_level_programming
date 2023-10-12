72c7453f19d69e6115004cbcc949893811b5cf1b
Oluwaseun <annyauthe4@gmail.com>
2023-10-11 20:56:48 -0700

    Print absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,12 +4,13 @@
  * _abs - prints the absolute value of an integer
  * @x: The integer to check.
  *
- * Return: int.
+ * Return: x
  */
 int _abs(int x)
 {
 	if (x < 0)
 	{
-		x = x * -1;
+		x = -x;
 		return (x);
+	}
 }
