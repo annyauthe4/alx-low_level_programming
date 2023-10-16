520071445efdff834cc482d868bc3ab4d4084fa9
Oluwaseun <annyauthe4@gmail.com>
2023-10-16 15:34:47 -0700

    Swap integer values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: ponter a
+ * @b: pinter b
+ *
+ * Return: int
+ */
+void swap_int(int *a, int *b)
+{
+	*a = &b;
+	*b = &a;
+}
