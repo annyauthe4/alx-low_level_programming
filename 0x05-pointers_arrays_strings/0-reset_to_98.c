d5c5beb960128a2a4793db864e16e6b0561268dd
Oluwaseun <annyauthe4@gmail.com>
2023-10-16 15:18:06 -0700

    Updates the variable value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer as parameter
+ * and updates the value
+ * @n: the pointer
+ *
+ * Return: int
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
