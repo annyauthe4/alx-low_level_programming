e03abe627182de048e3ffc2b8fb6e9c08969fda6
Oluwaseun <annyauthe4@gmail.com>
2023-10-17 09:57:53 -0700

    Print other part of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,8 +9,14 @@
 void puts2(char *str)
 {
 	int i;
+	int j = 0;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; i < j; i += 2)
 	{
 		_putchar(str[i]);
 	}
