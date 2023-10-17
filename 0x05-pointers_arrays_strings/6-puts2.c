633be14fd5e7eb04fbb4e4fe8d5cd450164ca055
Oluwaseun <annyauthe4@gmail.com>
2023-10-17 09:39:49 -0700

    Print other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints other character in a string
+ * @str: string to be treated
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
