7695d32c34f3872648a19df24feb2731a2663dc0
Oluwaseun <annyauthe4@gmail.com>
2023-10-19 21:48:56 -0700

    Compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,10 +9,37 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2)
+	int a = 0, b = 0, c = 0, r = 0, lim;
+
+	while (s1[a])
 	{
-		s1++;
-		s2++;
+		a++;
 	}
-	return (*s1 - s2);
+	while (s2[b])
+	{
+		b++;
+	}
+	if (a <= b)
+	{
+		lim = a;
+	}
+	else
+	{
+		lim = b;
+	}
+	while (c <= lim)
+	{
+		if (s1[c] == s2[c])
+		{
+			c++;
+			continue;
+		}
+		else
+		{
+			r = s1[c] - s2[c];
+			break;
+		}
+		c++;
+	}
+	return (r);
 }
