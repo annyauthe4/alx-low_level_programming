39fd73828477dd234ca2180ef2f326b146871fe4
Oluwaseun <annyauthe4@gmail.com>
2023-10-20 09:39:02 -0700

    Convert string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,12 +9,12 @@
 char *string_toupper(char *ptr)
 {
 	int i;
-	
+
 	for (i = 0; ptr[i] != '\0'; i++)
 	{
 		if (ptr[i] >= 'a' && ptr[i] <= 'z')
 		{
-			ptr[i] = ptr[i] - ('a' - 'A');
+			ptr[i] -= 'a' - 'A';
 		}
 	}
 	return (ptr);
