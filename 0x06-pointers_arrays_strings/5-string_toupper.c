c0e7ff8793b50404dc47d76e309a0bd68ccdecb9
Oluwaseun <annyauthe4@gmail.com>
2023-10-20 00:16:44 -0700

    Convert string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts string to uppercase
+ * @ptr: pointer to string
+ *
+ * Return: characters
+ */
+char *string_toupper(char *ptr)
+{
+	int i;
+
+	for (i = 0; ptr[i] != '\0'; i++)
+	{
+		if (i >= 'a' && i <= 'z')
+			ptr[i] = ptr[i] - 32;
+	}
+	return (ptr);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 Write a function that concatenates two strings
 Write a function that copies a string
 Write a function that compares two strings
+Write a function that changes all lowercase letters of a string to uppercase.
