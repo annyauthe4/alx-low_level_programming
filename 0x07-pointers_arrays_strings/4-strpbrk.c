089c7986618d6adcbd9594ffa68f6af2fe066a63
Oluwaseun <annyauthe4@gmail.com>
2023-10-23 14:43:30 -0700

    Search a string for any set of byte

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any set of byte
+ * @s: the string to search
+ * @accept: string to accept
+ *
+ * Return: pointer to s.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s != '\0')
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@ Write a function that fills memory with a constant byte.
 This program will write a function that copies a memory
 This program will write a function that gets the length of a prefix substring.
 This program will ger the length of a prefix substring
+This program will write a function that searches a string for any of a set of bytes.
