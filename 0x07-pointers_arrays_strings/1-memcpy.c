13a226e2966a636c9c7fc8cf66ce895f6d68228a
Oluwaseun <annyauthe4@gmail.com>
2023-10-23 09:44:22 -0700

    Copy memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,17 +8,13 @@
  *
  * Return: pointer to destination memory
  */
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *dest_ptr = dest;
-	char *src_ptr = src;
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		dest_ptr[i] = src_ptr[i];
-		dest_ptr++;
-		src_ptr++;
+		dest[i] = src[i];
 	}
 	return (dest);
 }
