a783ea97f70b590a1dee15a14a382108983ef81b
Oluwaseun <annyauthe4@gmail.com>
2023-10-23 14:57:15 -0700

    Search for any byte from a string

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,11 +11,11 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i;
 
-	while (*s)
+	while (*s != '\0')
 	{
 		for (i = 0; accept[i] != '\0'; i++)
 		{
-			if (*s == accept[i])
+			if (*s == accept[i] && *s != '\0')
 				return (s);
 		}
 		s++;
