e04e4ee095535db24918acef99e2456d07fa8ea9
Oluwaseun <annyauthe4@gmail.com>
2023-10-23 13:46:16 -0700

    Locate character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,17 +5,19 @@
  * @s: pointer to the string
  * @c: character to look for
  *
- * Return: s, the pointer to the string
+ * Return: pointer to the first occurrence
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s)
 	{
-		if (*s == c)
-		{
+		if (*s != c)
+			s++;
+		else
 			return (s);
-		}
-		s++;
 	}
+	if (c == '\0')
+		return (s);
+
 	return (NULL);
 }
