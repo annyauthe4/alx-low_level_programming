d22f9e4e213d8454d0fbd0c6c115ac86a2789528
Oluwaseun <annyauthe4@gmail.com>
2023-10-25 21:34:28 -0700

    Check if a string is a palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints the length of a string
+ * @s: character pointer
+ *
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * palindrome_ch - checks palindrome
+ * @s: the string to check
+ * @start: the beginning character
+ * @end: the end of the string
+ *
+ * Return: 1 or 0
+ */
+int palindrome_ch(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+	else if (s[start] != s[end])
+		return (0);
+	else
+		return (palindrome_ch(s, start + 1, end - 1));
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to check
+ *
+ * Return: 1 or 0
+ */
+int is_palindrome(char *s)
+{
+	return (palindrome_ch(s, 0, _strlen_recursion(s) - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,5 @@ int _sqrt_recursion(int n);
 int _sqrt_helper(int n, int guess);
 int is_prime_number(int n);
 int prime_checker(int n, int divisor);
+int is_palindrome(char *s)
+int palindrome_ch(char *s, int start, int end);
