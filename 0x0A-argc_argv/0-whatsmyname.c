908e3ee4913fe78cb63bee1b4469c1f72546eb9c
Oluwaseun <annyauthe4@gmail.com>
2023-10-27 21:21:09 -0700

    Write program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function run starts here
+ * @argc: argument count
+ * @argv: argument vector, an array of char
+ *
+ * Return: Always 0
+ */
+int main(argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+
+
+	return (0);
+}
