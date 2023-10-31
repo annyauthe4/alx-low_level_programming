a4c994df7b1c1888e268ba5f25a2d1c4b99cdb9c
Oluwaseun <annyauthe4@gmail.com>
2023-10-31 15:32:40 -0700

    Fix error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,8 +15,10 @@ char *str_concat(char *s1, char *s2)
 	unsigned int len2;
 	unsigned int i;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = " ";
+	if (s2 == NULL)
+		s2 = " ";
 	len1 = 0;
 	while (s1[len1] != '\0')
 	{
