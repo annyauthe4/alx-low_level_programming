d70e205faab6859ec8d2cacf3d6670e2dcd0a0cd
Oluwaseun <annyauthe4@gmail.com>
2023-10-31 17:45:39 -0700

    Free 2-D memory allocated

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees 2-D memory allocated
+ * @grid: pointer to a pointer to a grid
+ * @height: column of the grid
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+	int w;
+	int j;
+
+	grid = (int **)malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(w * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+	}
+}
