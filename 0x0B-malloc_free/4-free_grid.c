9fda923a9c05706df1ebd73a8c9b472d97381979
Oluwaseun <annyauthe4@gmail.com>
2023-10-31 18:05:31 -0700

    Fix bug

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -11,20 +11,13 @@
 void free_grid(int **grid, int height)
 {
 	int i;
-	int w;
-	int j;
 
-	grid = (int **)malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return;
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = (int *)malloc(w * sizeof(int));
-		if (grid[i] == NULL)
-		{
-			for (j = 0; j < i; j++)
-			{
-				free(grid[j]);
-			}
-			free(grid);
-		}
+		if (grid[i] != NULL)
+			free(grid[j]);
 	}
+	free(grid);
 }
