0d2c7a7416530e7d1dd53b263be59da7dc2ebc39
Oluwaseun <annyauthe4@gmail.com>
2023-11-03 15:35:32 -0700

    Fix the error

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -6,37 +6,35 @@
  * @ptr: pointer to previously allocated meomory
  * old_size: size in byte for ptr
  * @new_size: size in byte for the new memory block
- *
+ * @onl_size: size in byte for the old memory
  * Return: pointer to new memory block or NULL
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *arr;
 	unsigned int i, j;
-	
-	if (new_size > old_size)
-		arr;
+
 	if (new_size == old_size)
 		return (ptr);
-	ptr = malloc(old_size * sizeof(unsigned int));
-	arr = malloc((old_size + new_size) * sizeof(unsigned int));
-	if (arr == NULL)
-		return (NULL);
 	if (ptr == NULL)
+	{
+		arr = malloc(new_size);
 		return (arr);
-	for (i = 0; i <= old_size; i++)
+	}
+	if (new_size == 0)
+	{
+		free(got);
+		return(NULL);
+	}
+	arr = malloc(new_size);
+	if (arr == NULL)
+		return (arr);
+	if (new_size < old_size)
+		old_size = new_size;
+
+	for(i = 0; i < old_size; i++)
 	{
 		arr[i] = ptr[i];
 	}
-	for (j = 0; j <= new_size; j++)
-	{
-		arr[i + j] = j;
-	}
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
 	free(ptr);
 	return (arr);
-}
