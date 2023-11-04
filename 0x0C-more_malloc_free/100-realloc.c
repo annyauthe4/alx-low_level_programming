936a5912780cb941ddf519c98f56198e334f04ef
Oluwaseun <annyauthe4@gmail.com>
2023-11-03 20:07:25 -0700

    Fix error

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -4,15 +4,15 @@
 /**
  * _realloc - reallocates memory block
  * @ptr: pointer to previously allocated meomory
- * old_size: size in byte for ptr
+ * @old_size: size in byte for ptr
  * @new_size: size in byte for the new memory block
- * @onl_size: size in byte for the old memory
+ *
  * Return: pointer to new memory block or NULL
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned int i, j;
-	char *arr, *ptr;
+	unsigned int i;
+	char *arr, *old_ptr;
 
 	if (new_size == old_size)
 		return (ptr);
@@ -24,7 +24,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == 0)
 	{
 		free(ptr);
-		return(NULL);
+		return (NULL);
 	}
 	arr = malloc(new_size);
 	if (arr == NULL)
@@ -32,9 +32,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size < old_size)
 		old_size = new_size;
 
-	for(i = 0; i < old_size; i++)
+	for (i = 0; i < old_size; i++)
 	{
-		arr[i] = ptr[i];
+		arr[i] = old_ptr[i];
 	}
 	free(ptr);
 	return (arr);
