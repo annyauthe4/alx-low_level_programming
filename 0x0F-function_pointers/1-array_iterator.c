b27abcee10d0df1430aa67790a9633a8dc1006f1
Oluwaseun <annyauthe4@gmail.com>
2023-11-08 18:36:04 -0800

    Execute function in parameter

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - execute function given as parameter
+ * @array: An array of functions
+ * @size: size of the array
+ * @action: function pointer
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
