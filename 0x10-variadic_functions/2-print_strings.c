58a8575de3b84f896f0bcc415a52739d5785c256
Oluwaseun <annyauthe4@gmail.com>
2023-11-09 13:18:34 -0800

    Fix error

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(strs, int);
+		str = va_arg(strs, char*);
 		if (str == NULL)
 			str = "(nil)";
 		printf("%s", str);
