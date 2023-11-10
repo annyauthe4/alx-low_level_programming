9df3def0bd06e9924937f02454da6540b8bdbd08
Oluwaseun <annyauthe4@gmail.com>
2023-11-10 07:29:29 -0800

    Fix error

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,31 +10,30 @@ void print_all(const char * const format, ...)
 {
 	int i, flag;
 	char *str;
-	va_list all;
+	va_list a_list;
 
-	va_list(all, format);
+	va_start(a_list, format);
 	i = 0;
 	while (format != NULL && format[i] != '\0')
 	{
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(all, int));
+				printf("%c", va_arg(a_list, int));
 				flag = 0;
 				break;
 			case 'i':
-				printf("%i", va_arg(all, int));
+				printf("%i", va_arg(a_list, int));
 				flag = 0;
 				break;
 			case 'f':
-				printf("%f", va_arg(all, double));
+				printf("%f", va_arg(a_list, double));
 				flag = 0;
 				break;
 			case 's':
-				str = va_arg(all, char*);
+				str = va_arg(a_list, char*);
 				if (str == NULL)
 					str = "(nil)";
-
 				printf("%s", str);
 				flag = 0;
 				break;
@@ -44,9 +43,8 @@ void print_all(const char * const format, ...)
 		}
 		if (format[i + 1] != '\0' && flag == 0)
 			printf(", ");
-
 		i++;
 	}
 	printf("\n");
-	va_end(all);
+	va_end(a_list);
 }
