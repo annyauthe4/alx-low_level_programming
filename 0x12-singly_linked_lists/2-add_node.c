b929ad7dcd468e8780b0cba86834eeda30d88b37
Otetumo <annyauthe4@gmail.com>
2023-11-17 00:15:13 +0100

    Fix error

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,6 +4,22 @@
 #include <stddef.h>
 
 /**
+  * _strlen - counts string lenght
+  * @s: pointer to string to be counted
+  * Return: number of string
+  */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
   * add_node - adds a new node at the beginning of a list_s
   * @head: pointer to a pointer to structured linked list
   * @str: pointer to a string
@@ -15,18 +31,11 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (str == NULL)
 		return (NULL);
-
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
+	new_node->len = _strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
