080efa41494c5bbea0acef1b2a90c7411e0225d6
Otetumo <annyauthe4@gmail.com>
2023-11-17 17:06:31 +0100

    Fix header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <stddef.h>
 
 /**
@@ -22,6 +24,6 @@ typedef struct list_s
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-ist_t *add_node(list_t **head, const char *str);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
