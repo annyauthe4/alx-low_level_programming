4a44b9a49d556f18c61d6b0527e2ccce871cf4c2
Otetumo <annyauthe4@gmail.com>
2023-11-21 18:10:43 +0100

    Fix error

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,14 +12,7 @@ size_t print_listint(const listint_t *h)
 	count = 0;
 	while (h != NULL)
 	{
-		if (h->next == NULL)
-		{
-			printf("NULL\n");
-		}
-		else
-		{
-			printf("%d\n", h->n);
-		}
+		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
