0c9e3c4e58993d752ff19d1511fdc1f3a6d7de87
Otetumo <annyauthe4@gmail.com>
2023-11-22 06:04:58 +0100

    Fix error

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,9 +15,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	if (index == 0)
 	{
-		prev = (*head)->next;
-		free(*head);
-		*head = prev;
+		temp = (*head);
+		*head = (*head)->next;
+		free(temp);
 		return (1);
 	}
 	temp = *head;
