8627da1d5e8ef3468272f5a6e5a396f9840fe8e7
Otetumo <annyauthe@yahoo.com>
2024-06-27 18:30:22 +0100

    Add main header file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,3 +14,5 @@ void times_table(void);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
+
+#endif
