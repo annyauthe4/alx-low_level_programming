7599a97f1da21ed3ad7a3255c7f1bc620995738b
Otetumo <annyauthe@yahoo.com>
2024-06-27 23:17:51 +0100

    Print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,10 +13,11 @@ void print_diagonal(int n)
 		_putchar('\n');
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j <= i + 1; j++)
+		for (j = 0; j < i; j++)
 		{
-			_putchar(j < i + 1 ? ' ' : '\\');
+			_putchar(' ');
 		}
+		_putchar('\\');
 		_putchar('\n');
 	}
 }
