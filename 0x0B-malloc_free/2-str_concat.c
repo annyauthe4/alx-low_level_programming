871f6cf25f51065656fbec9a59a90a5faea72e00
Otetumo <annyauthe@yahoo.com>
2024-07-15 22:48:38 +0100

    Concatenate two strings in allocated memory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * str_concat - concatenates two strings
@@ -11,9 +10,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *arr;
-	unsigned int len1;
-	unsigned int len2;
-	unsigned int i;
+	unsigned int i, len1, len2;
 
 	if (s1 == NULL)
 		s1 = " ";
@@ -21,25 +18,21 @@ char *str_concat(char *s1, char *s2)
 		s2 = " ";
 	len1 = 0;
 	while (s1[len1] != '\0')
-	{
 		len1++;
-	}
+
 	len2 = 0;
 	while (s2[len2] != '\0')
-	{
 		len2++;
-	}
+
 	arr = (char *)malloc(len1 + len2 + 1);
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i <= len1; i++)
-	{
 		arr[i] = s1[i];
-	}
+
 	for (i = 0; i <= len2; i++)
-	{
 		arr[len1 + i] = s2[i];
-	}
+
 	arr[len1 + len2] = '\0';
 	return (arr);
 }
