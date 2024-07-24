65ea6e82121dad2048b1b6e00a691c73833b406e
annyauthe4 <annyauthe@yahoo.com>
2024-07-24 20:18:33 +0100

    Perform simple mathematical operation

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,50 +1,41 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
  * op_add - adds two integers
  * @a: first number
  * @b: 2nd number
- *
- * Return: sum
+ * Return: int on success
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
 /**
- * op_sub - finds the difference between 2 numbers
- * @a: 1st number
- * @b: 2nd number
- *
- * Return: difference
- */
+  * op_sub - Finds the difference between two integers
+  * @a: First number
+  * @b: 2nd number
+  * Return: int on success
+  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
 /**
- * op_mul - multiplies two numbers
- * @a: 1st number
- * @b: 2nd number
- *
- * Return: multiplication
- */
+  * op_mul - multiplies two integers
+  * @a: first number
+  * @b: 2nd number
+  * Return: int on success
+  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
 /**
- * op_div - divides two numbers
- * @a: 1st number
- * @b: 2nd number
- *
- * Return: division
- */
+  * op_div - Divides 1st integer by the 2nd
+  * @a: First number
+  * @b: 2nd number
+  * Return: int on success
+  */
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -54,19 +45,18 @@ int op_div(int a, int b)
 	}
 	return (a / b);
 }
-
 /**
- * op_mod - returns the remainder of a division
- * @a: 1st number
- * @b: 2nd number
- *
- * Return: modulo
- */
+  * op_mod - Finds the modulus of one int by the other
+  * @a: First num
+  * @b: 2nd num
+  * Return: int on success
+  */
 int op_mod(int a, int b)
 {
 	if (b == 0)
 	{
 		printf("Error\n");
+		return (100);
 	}
 	return (a % b);
 }
