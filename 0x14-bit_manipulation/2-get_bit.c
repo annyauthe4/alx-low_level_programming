b5e8133430dcadfa18836ba79e667eb000fff693
Otetumo Oluwaseun <annyauthe@yahoo.com>
2024-08-09 05:29:49 +0100

    Get bit at index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,5 +13,5 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 	mask = 1UL << index;
-	return(((n & mask) != 0) ? 1 : 0);
+	return (((n & mask) != 0) ? 1 : 0);
 }
