cfe175eb1961df4963dac015f6869b88b8691611
Otetumo Oluwaseun <annyauthe@yahoo.com>
2024-08-15 23:40:09 +0100

    Fix read error exit code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,46 @@
 #include "main.h"
-
+/**
+  * check97 - Checks if argc is exactly 3
+  * @argc: Argument count
+  * Return: void.
+  */
+void check97(int argc)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, ARGC_ERROR);
+		exit(97);
+	}
+}
+/**
+  * check_fd_from - Checks if file descriptor of file1 fail
+  * @fd: fd of file1
+  * @file_from: The file1 name
+  * Return: void
+  */
+void check_fd_from(int fd, char *file_from)
+{
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, READ_ERROR, file_from);
+		exit(98);
+	}
+}
+/**
+  * check_fd_to - Checks the return of fd_to
+  * @fd: fd for the file to copy to
+  * @file_to: Pointer to file2
+  * Return: void
+  */
+void check_fd_to(int fd, char *file_to)
+{
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, WRITE_ERROR, file_to);
+		close(fd);
+		exit(99);
+	}
+}
 /**
   * main - Program copies the content of a file into another file
   * @argc: Argument count
@@ -13,26 +54,13 @@ int main(int argc, char *argv[])
 	char buffer[BUFFER_SIZE];
 	mode_t old_umask;
 	
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, ARGC_ERROR);
-		exit(97);
-	}
+	check97(argc);
 	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, READ_ERROR, argv[1]);
-		exit(98);
-	}
+	check_fd_from(fd_from, argv[1]);
 	old_umask = umask(0);
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	umask(old_umask);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, WRITE_ERROR, argv[2]);
-		close(fd_to);
-		exit(99);
-	}
+	check_fd_to(fd_to, argv[2]);
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
@@ -49,7 +77,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, READ_ERROR, argv[1]);
 		close(fd_from);
 		close(fd_to);
-		exit(99);
+		exit(98);
 	}
 	if (close(fd_from) == -1)
 	{
