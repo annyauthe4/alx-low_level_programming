bf49357a5dd35a679682daa4f332fb01b22c7610
Otetumo Oluwaseun <annyauthe@yahoo.com>
2024-10-11 18:24:31 +0100

    Create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,26 +1,6 @@
 #include "hash_tables.h"
 
 /**
-  * hash_function - maps keys to values.
-  * @str: Pointer to key string.
-  * @capacity: The size of the hash table
-  *
-  * Return: unsigned long index of the key.
-  */
-unsigned long hash_function(char *str, unsigned long capacity)
-{
-	unsigned long hash;
-	int c;
-
-	hash = 5381;
-	c = 0;
-	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c;
-	}
-	return (hash % capacity);
-}
-/**
   * hash_table_create - Creates a new hash table of certain size
   * @size: The size/capacity of the hash table.
   *
@@ -34,9 +14,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = calloc(table->size, sizeof(hash_node_t));
+	table->array = calloc(table->size, sizeof(hash_node_t *));
 
 	if (table->array == NULL)
+	{
+		free(table);
 		return (NULL);
+	}
 	return (table);
 }
