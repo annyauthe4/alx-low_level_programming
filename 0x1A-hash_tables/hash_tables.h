fe9d23929e07f3351bf55746ebc766a1fe3fa8d4
Otetumo Oluwaseun <annyauthe@yahoo.com>
2024-10-11 18:40:32 +0100

    Create hash table function

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -34,7 +34,7 @@ typedef struct hash_table_s
      hash_node_t **array;
 } hash_table_t;
 
-unsigned long hash_function(char* str, unsigned long capacity);
+unsigned long int hash_djb2(const unsigned char *str);
 hash_table_t *hash_table_create(unsigned long int size);
 
 
